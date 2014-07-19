ad1876c91b1c640006217a20f9f38b59d7dce17b
Martin Feckie <mfeckie@gmail.com>
2014-07-19 21:14:20 +0800

    Add more detail to senators in list response

--- lib/openaustralia/senators.rb
+++ lib/openaustralia/senators.rb
@@ -36,9 +36,20 @@ class SenatorFound
   include XML::Mapping
 
   numeric_node :member_id, 'member_id'
-  text_node :name, 'name'
-  numeric_node :person_id, 'person_id'
+  numeric_node :house, 'house'
+  text_node :first_name, 'first_name'
+  text_node :last_name, 'last_name'
+  text_node :constituency, 'constituency'
   text_node :party, 'party'
+  text_node :entered_house, 'entered_house'
+  text_node :entered_reason, 'entered_reason'
+  text_node :left_house, 'left_house'
+  text_node :left_reason, 'left_reason'
+  numeric_node :person_id, 'person_id'
+  text_node :title, 'title'
+  text_node :full_name, 'full_name'
+  text_node :name, 'name'
+  
 end
 
 # record for foresults of a representative search
